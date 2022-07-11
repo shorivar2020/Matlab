fd78420113325415fdd269f4088c9e493da5ae7d
shorivar2020 <101527748+shorivar2020@users.noreply.github.com>
2022-07-12 00:16:03 +0200

    matlab

--- ar_fit_model.m
+++ ar_fit_model.m
@@ -1,5 +1,4 @@
 function [a] = ar_fit_model(y, p)
-%[y, ~] = audioread('gong.wav');
 T = length(y)-1;
 W = p;
 H = T-p+1;
@@ -7,8 +6,6 @@ b = y(W+1:T+1);
 L = nan(H, W);
 for i = 1:(H)
     for j = 1:(W)
-        %L(i, j) = y(T-p-j+i);
-        %L(i, j) = y(W+i-1-j+1);
         L(i, j) = y(p+i-j);
     end
 end
